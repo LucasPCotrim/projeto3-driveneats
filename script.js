1b20fafae0973fcb1558b64adda1482ac782f9bf
lucas_cotrim <lucas_cotrim@tpn.usp.br>
2022-06-08 20:23:52 -0300

    First js commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function highlight_A(opt) {
+    document.getElementById(opt).style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+}
+function highlight_B(opt) {
+    document.getElementById(opt).style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+}
+function highlight_C(opt) {
+    document.getElementById(opt).style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+}
