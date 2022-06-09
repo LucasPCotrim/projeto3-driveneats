054f2278977086151f45fb4181f8bbc910a7bbde
lucas_cotrim <lucas_cotrim@tpn.usp.br>
2022-06-08 21:53:12 -0300

    Implemented option selection logic

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-function highlight_A(opt) {
-    document.getElementById(opt).style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+let options_selected = {'A': [0, 0, 0, 0],
+                             'B': [0, 0, 0, 0],
+                             'C': [0, 0, 0, 0]};
+
+
+
+function highlight(order_menu_index, order_menu_option) {
+    update_options_selected(order_menu_index, order_menu_option)
+
+    let x = document.getElementById(order_menu_index).querySelectorAll(".order_option");
+
+    for (let i = 0; i < options_selected[order_menu_index].length; i++) {
+        if (options_selected[order_menu_index][i] == 1){
+            x[i].style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
+        }
+        else{
+            x[i].style.boxShadow = "initial";
+        }
+        
+    }
+    
 }
-function highlight_B(opt) {
-    document.getElementById(opt).style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
-}
-function highlight_C(opt) {
-    document.getElementById(opt).style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+
+
+
+function update_options_selected(order_menu_index, order_menu_option){
+
+    options_selected[order_menu_index] = [0, 0, 0, 0];
+    options_selected[order_menu_index][order_menu_option] = 1;
+    
 }
